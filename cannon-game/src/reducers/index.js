6938b603bfb66b5d554723932ed4a14a30b06cde
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-08-09 11:42:08 +0100

    actions folder set up with move-objects

--- cannon-game/src/reducers/index.js
+++ cannon-game/src/reducers/index.js
@@ -1,9 +1,16 @@
-const initialState ={
-  message: "integrate React and Redux",
+import { MOVE_OBJECTS } from '../actions';
+
+const initialState = {
+  angle: 45,
 };
 
-function reducer(state = initialState) {
-  return state;
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS: 
+      return moveObjects( state, action );
+    default:
+      return state;
+  }
 }
 
 export default reducer;
