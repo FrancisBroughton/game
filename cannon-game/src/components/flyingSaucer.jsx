a680b6571c26670626f811600d4e3709c49a66fd
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-08-13 15:30:18 +0100

    the flyingSacuvers now moving down the screen

--- cannon-game/src/components/flyingSaucer.jsx
+++ cannon-game/src/components/flyingSaucer.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
 import FlyingSaucerBase  from './flyingSaucerBase';
 import FlyingSaucerTop  from './flyingSaucerTop';
+import { gameHeight } from '../utils/constants';
+
+const moveVertically = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(${gameHeight}px);
+  }
+`;
+
+const Move = styled.g`
+  animation ${moveVertically} 4s linear;
+`;
+
 
 const FlyingSaucer = (props) => (
-  <g>
+  <Move>
     <FlyingSaucerBase position={props.position} />
     <FlyingSaucerTop position={props.position} />
-  </g>
+  </Move>
 );
 
 FlyingSaucer.PropTypes = {
--- cannon-game/src/utils/constants.js
+++ cannon-game/src/utils/constants.js
@@ -4,13 +4,15 @@ export const gameFullWidth = 5000;
 
 export const gameWidth = 800;
 
+export const gameHeight = 1200
+
 export const createInterval = 1000;
 //object every 1000 milliseconds
 
 export const maxFlyingObjects = 4;
 //4 objects per time
 
-export const flyingObjectsStarterYAxis = -1000;
+export const flyingObjectsStarterYAxis = -1100;
 
 
 export const flyingObjectsStarterPositions = [
